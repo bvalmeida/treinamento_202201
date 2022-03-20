2b1a9e9114b1dfdb6bde60e2dddfed4c620371de
bvalmeida <95421375+bvalmeida@users.noreply.github.com>
2022-03-20 10:13:21 -0300

    implementado busca por nome

--- model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
+++ model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
@@ -6,7 +6,11 @@ import com.indracompany.treinamento.model.entity.Cliente;
 
 public interface ClienteRepository extends GenericCrudRepository<Cliente, Long>{
 	
+	Cliente findTopByCpfOrderByIdDesc(String cpf);
+	
 	List<Cliente> findByCpf(String cpf);
+	
+	List<Cliente> findByNomeContaining(String nome);
 	
 
 }
