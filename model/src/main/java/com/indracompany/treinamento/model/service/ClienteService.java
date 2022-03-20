5f9d420be83ac020ff268e8c546bbdf8f0c8eccd
bvalmeida <95421375+bvalmeida@users.noreply.github.com>
2022-03-20 10:15:11 -0300

    implementado busca por nome

--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -15,26 +15,76 @@ import com.indracompany.treinamento.util.CpfUtil;
 @Service
 public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRepository>{
 
-	  public List<ClienteDTO> buscarClientePorCpf(String cpf) {
-		  boolean cpfValido = cpf != null && CpfUtil.validaCPF(cpf);
-		  
-		  if (!cpfValido) {
-			  throw new AplicacaoException(ExceptionValidacoes.ERRO_CPF_INVALIDO);
-		  }
-		  
-		  List<Cliente> clientes = repository.findByCpf(cpf);
-		  
-		  if (clientes == null || clientes.isEmpty()) {
-			  throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO);
-		  }
-		  
-		  List<ClienteDTO> retorno = new ArrayList<ClienteDTO>();
-		  for (Cliente c: clientes) {
-			  ClienteDTO dto = new ClienteDTO();
-			  dto.setEmail(c.getEmail());
-			  dto.setNome(c.getNome());
-			  retorno.add(dto);
-		  }
-		  return retorno;
-	  }
+	public ClienteDTO buscarClientePorCpf(String cpf) {
+		
+		boolean cpfValido = cpf != null && CpfUtil.validaCPF(cpf);
+		
+		if(!cpfValido) {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_CPF_INVALIDO);
+		}
+		
+		Cliente c = repository.findTopByCpfOrderByIdDesc(cpf);
+		
+		if(c == null) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO);
+		}
+		
+		ClienteDTO retorno = new ClienteDTO();
+		retorno.setEmail(c.getEmail());
+		retorno.setNome(c.getNome());
+		
+		return retorno;
+	}
+	
+	public List<ClienteDTO> buscarClientePorCpfLista(String cpf) {
+		
+		boolean cpfValido = cpf != null && CpfUtil.validaCPF(cpf);
+		
+		if(!cpfValido) {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_CPF_INVALIDO);
+		}
+		
+		List<Cliente> clientes = repository.findByCpf(cpf);
+		
+		if(clientes == null || clientes.isEmpty()) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO);
+		}
+		
+		List<ClienteDTO> retorno = new ArrayList<ClienteDTO>();
+		for(Cliente c: clientes) {
+			ClienteDTO dto = new ClienteDTO();
+			dto.setEmail(c.getEmail());
+			dto.setNome(c.getNome());
+			retorno.add(dto);
+		}
+		
+		return retorno;
+	}
+	
+	public List<ClienteDTO> buscarClientePorNome(String nome){
+		
+		boolean nomeValido = nome != null;
+		
+		if(!nomeValido) {
+			throw new AplicacaoException(ExceptionValidacoes.ERRO_OBJETO_NAO_ENCONTRADO);
+		}
+		
+		List<Cliente> clientes = repository.findByNomeContaining(nome);
+		
+		if(clientes == null || clientes.isEmpty()) {
+			throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO);
+		}
+		
+		List<ClienteDTO> retorno = new ArrayList<ClienteDTO>();
+		for(Cliente c: clientes) {
+			ClienteDTO dto = new ClienteDTO();
+			dto.setNome(c.getNome());
+			dto.setEmail(c.getEmail());
+			retorno.add(dto);
+		}
+		
+		return retorno;
+		
+	}
+	
 }
